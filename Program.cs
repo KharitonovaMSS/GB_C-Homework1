60e8d39db2d2ea3a144ebe3dee711937a84eb72b
Maria <mineeva_maria@mail.ru>
2022-11-12 14:21:11 +0300

    Сравнение 2 чисел

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число A: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+    Console.WriteLine("Число A больше числа B");
+if (number2 > number1)
+    Console.WriteLine("Число B больше числа A");
+if (number1 == number2)
+    Console.WriteLine("Число А равно числу Б");
